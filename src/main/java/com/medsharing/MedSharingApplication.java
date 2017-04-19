2f86a80e261719e6261cdf16b667e1353c457fab
dandelionxxj <1559558904@qq.com>
2017-04-19 12:19:18 +0800

    start

---
+++ src/main/java/com/medsharing/MedSharingApplication.java
@@ -0,0 +1,12 @@
+package com.medsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedSharingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/medsharing/MedSharingApplicationTests.java
@@ -0,0 +1,18 @@
+package com.medsharing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MedSharingApplication.class)
+@WebAppConfiguration
+public class MedSharingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
